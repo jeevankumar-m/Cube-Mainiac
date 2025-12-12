6a6ca69e0565ce2019f6d95a47241655ef64caeb
jeevankumar-m <jeevankumar06m@gmail.com>
2025-12-12 19:40:41 +0530

    added game over logic and new assets for tiles and character

--- main.py
+++ main.py
@@ -11,10 +11,12 @@ clock = pygame.time.Clock()
 
 player_img = pygame.image.load('player.png') 
 dirt_img = pygame.image.load('dirt.png')
+dirt2_img = pygame.image.load('dirt2.png')
+game_over_img = pygame.image.load('gameover.png')
 
 TILE_SIZE = dirt_img.get_width()
 
-player_rect = pygame.Rect(100, 100, player_img.get_width(), player_img.get_height())
+player_rect = pygame.Rect(0 * TILE_SIZE, 24 * TILE_SIZE, player_img.get_width(), player_img.get_height())
 def load_map(path):
     f = open(path + '.txt', 'r')
     data = f.read()
@@ -63,9 +65,11 @@ moving_right = False
 moving_left = False 
 true_scroll = [0, 0]
 
+running = True
+while running:
+    display.fill((72, 77, 77))
+    pygame.draw.rect(display, (43, 46, 46), pygame.Rect(0, 150, 350, 125))
 
-while True:
-    display.fill((255, 255, 255))
 
     true_scroll[0] += (player_rect.x - true_scroll[0] - 191) / 20 
     true_scroll[1] += (player_rect.y - true_scroll[1] - 141) / 20 
@@ -76,14 +80,15 @@ while True:
 
     player_movement = [0, 0]
     if moving_right == True:
-        player_movement[0] += 2
+        player_movement[0] += 3
     if moving_left == True:
-        player_movement[0] -= 2
+        player_movement[0] -= 3
 
     player_movement[1] += player_y_momentum 
     player_y_momentum += 0.2
 
     tile_rects = []
+    game_over_rect = []
     y = 0 
     for row in game_map:
         x = 0
@@ -91,13 +96,19 @@ while True:
             if tile == '1': 
                 display.blit(dirt_img, (x * TILE_SIZE - scroll[0], y * TILE_SIZE - scroll[1]))
             if tile == '2':
-                display.blit(dirt_img, (x * TILE_SIZE - scroll[0], y * TILE_SIZE - scroll[1]))
-            if tile != '0':
+                display.blit(dirt2_img, (x * TILE_SIZE - scroll[0], y * TILE_SIZE - scroll[1]))
+            if tile == '3':
+                display.blit(game_over_img, (x * TILE_SIZE - scroll[0], y * TILE_SIZE - scroll[1]))
+                game_over_rect.append(pygame.Rect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE))
+            if tile != '0' and tile != '3':
                 tile_rects.append(pygame.Rect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE))
             x += 1 
         y += 1
 
     player_rect, collisions = move(player_rect, player_movement, tile_rects)
+
+    if player_rect.colliderect(game_over_rect[0]):
+        running = False
 
     if collisions['bottom']:
         player_y_momentum = 0 
@@ -107,7 +118,6 @@ while True:
     else:
         air_timer += 1
     
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit() 
